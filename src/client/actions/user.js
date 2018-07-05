2a58f082309c24fe8f8d1f47dd4c814ca3b7d45b
Alexandre HOAREAU <ahoareau@e1r5p9.42.fr>
2018-07-05 18:41:58 +0200

    wip data passe pas

--- src/client/actions/user.js
+++ src/client/actions/user.js
@@ -1,7 +1,6 @@
 import { push } from 'connected-react-router'
 import { SubmissionError } from 'redux-form'
 
-export const USER_SIGN_UP = 'user/SIGN_UP'
 export const USER_LOGIN = 'user/LOGIN'
 
 const initialState = {
@@ -10,22 +9,10 @@ const initialState = {
   role: '',
 }
 
-export const signUp = user => ({
-  type: USER_SIGN_UP,
-  globalLoading: true,
-  apiCall: Api.signUp(user),
-  catchFn: e => {
-    throw new SubmissionError({
-      _error: e.message && e.message,
-      ...e
-    })
-  }
-})
-
 export const login = user => ({
   type: USER_LOGIN,
   globalLoading: true,
-  apiCall: Api.login(user),
+  apiCall: () => 'is logged',
   thenFn: dispatch => {
     dispatch(push('/'))
   },
