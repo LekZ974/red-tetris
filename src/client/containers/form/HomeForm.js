5ad22c91fae53b17d0d1aa536db0ef653a67d67f
Alexandre HOAREAU <ahoareau@e1r5p9.42.fr>
2018-07-05 18:41:58 +0200

    wip data passe pas

--- src/client/containers/form/HomeForm.js
+++ src/client/containers/form/HomeForm.js
@@ -2,21 +2,26 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { compose } from 'recompose'
 import {Input, Error, Button} from "../../components/block";
+import {login} from "../../actions/user";
+
+const onSubmit = values => console.log(values)
 
 const HomeForm = ({ handleSubmit, error, submitting }) => (
-<form>
+<form onSubmit={handleSubmit(onSubmit)}>
   <Field
     placeholder='Your name'
     name='name'
     component={Input}
+    type={'text'}
   />
   <Field
     placeholder={'Your Party\'s name or select one in the list'}
     name='room'
     component={Input}
+    type={'text'}
   />
   <Error />
-  <Button size={'large'} fullWidth to='/new-game'>Go</Button>
+  <Button type={'submit'} size={'large'} fullWidth to='/new-game'>Go</Button>
 </form>
 )
 export default compose(
