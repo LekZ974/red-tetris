f859195080130047b9ebb182da24cba6d5eb9b0c
Graziella HIPPODAM <ghippoda@e1r6p11.42.fr>
2018-07-05 18:39:17 +0200

    Start to build Room's components

---
+++ src/client/components/Room/GameInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class GameInfo extends Component {
+    constructor(props){
+      super(props)
+    }
+
+    render(){
+      return(
+        <div>GameInfo</div>
+      )
+    }
+}
+
+export default GameInfo
--- src/client/containers/view/Room.js
+++ src/client/containers/view/Room.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Box, Card } from '../../components/block'
 import { Route } from 'react-router-dom';
+import RoomInfo from '../../components/Room/RoomInfo'
+import GameInfo from '../../components/Room/GameInfo'
+import PlayGround from '../../components/Room/PlayGround'
 
 const Room = ({ match, props }) => {
   console.log(props)
   return(
     <Box width={'100%'} flex flexDirection='row' justifyContent='center'>
       <Card flex={1} width={'40em'}>
-        <Box fontSize={30}>
-          A ROOM
-        </Box>
+        <RoomInfo />
+      </Card>
+      <Card flex={1} width={'40em'}>
+        <PlayGround />
+      </Card>
+      <Card flex={1} width={'40em'}>
+        <GameInfo />
       </Card>
       <Route path={`${match.path}/:user`} render= {({match}) =>( <div> <h3> {match.params.name} </h3></div>)}/>
     </Box>
