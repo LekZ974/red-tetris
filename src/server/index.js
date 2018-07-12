b61390eb6242b958f45858a05836e1d32d315e3f
Jennifer WONG <jwong@e2r7p8.42.fr>
2018-07-12 18:06:39 +0200

    init player upon user connection with socket

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import params from '../../params'
 import routes from './constants/routes'
 import getShape from './eventHandlers/tetriminos'
+import Player from './controllers/player'
 
 const app = express()
 const server = require('http').createServer(app)
@@ -20,8 +21,10 @@ app.get('/', (req, res) => {
 
 io.on('connection', (client) => {
     console.log('client has connected ')
+	let player = new Player('123', client.id)
+	console.log('socket id = ', player.getSocketID())
     client.on(routes.REQUEST_SHAPE, (userID) => {
-        console.log('getting shape: ')
+        console.log('getting shape ')
         client.emit(routes.EMITTED_SHAPE, getShape(userID))
     })
     client.on('disconnect', () => {
