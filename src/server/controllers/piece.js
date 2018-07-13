bfdaf5f118dc04f2fc10ee1a926b233361861397
Jennifer WONG <jwong@e2r7p7.42.fr>
2018-07-13 16:10:06 +0200

    add class piece

---
+++ src/server/controllers/piece.js
@@ -0,0 +1,33 @@
+export default class Piece {
+	constructor(shape) {
+		this.shape = shape
+		this.rotation = [0, 90, 180, 270]
+		this.transpose = ['none', 'left-right', 'up-down']
+	}
+
+	getShape() {
+		return this.shape
+	}
+
+	rotateShape(matrix) {
+		let rotated = matrix[0].map((col, c) => {
+			return matrix.map((row, r) => {
+				return matrix[r][c]
+			})
+		})
+
+		return rotated
+	}
+
+	rotate(deg, matrix) {
+		let i = 0;
+		let tmp = matrix
+
+		while (i <= deg) {
+			tmp = this.rotateShape(tmp)
+			i += 90
+		}
+		return tmp
+			
+	}
+}
