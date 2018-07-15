9936e419b24881b1ab990660a62c81f83ee8d750
kernelj <jenniferwong89@gmail.com>
2018-07-15 19:08:03 +0200

    add game class

---
+++ src/server/controllers/game.js
@@ -0,0 +1,33 @@
+export default class Game {
+    constuctor(shapes) {
+        this.shapes = shapes
+        this.master = null
+        this.challenger = null
+        this.roomID = ''
+        this.roomName = ''
+        this.gameStarted = false
+    }
+
+    setRoomInfo(id, name) {
+        this.roomID = id
+        this.roomName = name
+    }
+
+    setMaster(master) {
+        this.master = master
+    }
+
+    setChallenger(challenger) {
+        this.challenger = challenger
+    }
+
+    setGameStarted() {
+        this.gameStarted = true
+    }
+
+    waitingForPlayers() {
+        if (this.master && this.challenger)
+            return false
+        return true
+    }
+}
