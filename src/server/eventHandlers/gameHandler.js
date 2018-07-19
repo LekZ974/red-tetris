55377ad11a8da8cba94803c0d8c66af62fb1ca0f
Jennifer WONG <jwong@e3r2p8.42.fr>
2018-07-19 16:52:12 +0200

    add gameHandler

---
+++ src/server/eventHandlers/gameHandler.js
@@ -0,0 +1,44 @@
+import shapes from '../constants/shapes'
+
+import Piece from '../controllers/piece'
+import Game from '../controllers/game'
+
+const findPlayer = function(socketId, users) {
+	const player = users.find((id) => {
+		if (id.socketID === socketId) {
+			return id
+		}
+	})
+	return player
+}
+
+const findGame = function(gameId, allGames) {
+	const game = allGames.find((id) => {
+		if (id.roomID === gameId)
+			return id
+	})
+	return game
+}
+
+const initShapes = function() {
+	let shapeArray = []
+
+	shapes.forEach((elem) => {
+		shapeArray.push(new Piece(elem))
+	})
+	return shapeArray
+}
+
+const createGame = function(socketId, users) {
+	const master = findPlayer(socketId, users)
+	const tetriminos = initShapes()
+	let game = new Game(tetriminos)
+
+	game.setMaster(master)
+	return game
+}
+
+export {
+	findGame,
+	createGame
+}
