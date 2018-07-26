23e8c57ca0565b405debcf659a853f91a10a0eb5
Jennifer Wong <jenniferwong89@gmail.com>
2018-07-26 18:01:08 +0200

    add game exists to routes

--- src/server/constants/routes.js
+++ src/server/constants/routes.js
@@ -1,6 +1,7 @@
 const routing = {
     LOGIN: 'login',
     CREATE_GAME: 'createGame',
+    GAME_EXISTS: 'gameExists',
     JOIN_GAME: 'joinGame',
     REQUEST_SHAPE: 'requestShape',
     EMITTED_SHAPE: 'emittedShape'
