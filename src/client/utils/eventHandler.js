5c0a7852d302ecdcc9b3dcef6ea17a59cc799f8a
Alexandre HOAREAU <ahoareau@e2r2p9.42.fr>
2018-10-15 15:50:16 +0200

    fix

--- src/client/utils/eventHandler.js
+++ src/client/utils/eventHandler.js
@@ -2,7 +2,7 @@ import {tetriAction} from "../actions/tetrimino";
 import {store} from "../index";
 
 const eventHandler = (event) => {
-  if (store.getState().user.name && store.getState().game.gameName) {
+  if (store.getState().user.name && store.getState().game.name) {
     event.preventDefault()
     store.dispatch(tetriAction(event.code))
   }
