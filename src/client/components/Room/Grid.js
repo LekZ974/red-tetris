6488dc181193cb02451f974e36e40aa88732fce9
Alexandre Hoareau <alexandrehoareau@MacBook-Pro-de-Alexandre.local>
2018-10-15 15:51:48 +0200

    refacto wip

---
+++ src/client/components/Room/Grid.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import * as TetriService from "../../services/TetriService"
+import {connect} from "react-redux";
+
+const GridUserComponent = (state) => {
+  return(
+    state.gridRender.map((row, key) =>{
+        return(<div key={key}>{row}</div>)
+      }))
+}
+
+const mapStateToProps = state => {
+
+  /* PLAYERGRID */
+
+  const gridRender = [];
+  const userState = state.user;
+  let playerGrid = userState.grid.map(l => l.map(e => e));
+
+  if (state.game.gameIsStarted) {
+    console.log("TETRIMINO", state.tetrimino)
+    playerGrid = TetriService.placePiecePreview(playerGrid, state.tetrimino);
+    playerGrid = TetriService.placePiece(playerGrid, state.tetrimino);
+  }
+
+  playerGrid.forEach(l => {
+    gridRender.push([...l]);
+  });
+  return {
+    userState: Object.assign({}, state.user),
+    gridRender: gridRender,
+  }
+};
+
+const Grid = connect(
+  mapStateToProps,
+  undefined
+)(GridUserComponent);
+
+export {Grid};
