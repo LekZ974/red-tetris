3a442efe9db1dc22dbb954c48820b34cb0530ac3
kernelj <jenniferwong89@gmail.com>
2018-10-16 23:58:53 +0200

    add tests for route handling and game handling

--- tests/server/eventHandlers/routeHandler.test.js
+++ tests/server/eventHandlers/routeHandler.test.js
@@ -31,6 +31,7 @@ test('getGames', () => {
 test('createGame', () => {
 	let player1 = { id : 0 }
 	let player2 = { id : 1 }
+
 	let logged1 = routeHandler.login(player1, player1, onlineUsers)
 	let logged2 = routeHandler.login(player2, player2, onlineUsers)
 
@@ -45,6 +46,86 @@ test('createGame', () => {
 
 		expect(result).toMatch('KO')
 	}
+})
+
+test('joinGame', () => {
+	let player1 = { id : 0 }
+	let player2 = { id : 1 }
+	let player3 = { id : 2 }
+
+	let logged1 = routeHandler.login(player1, player1, onlineUsers)
+	let logged2 = routeHandler.login(player2, player2, onlineUsers)
+	let logged3 = routeHandler.login(player3, player3, onlineUsers)
+
+	if (logged1 === 'OK' && logged2 === 'OK' && logged3 === 'OK') {
+		let game = routeHandler.createGame(player1, activeGames, onlineUsers, 'GAME1')
+
+		if (game === 'OK') {
+			expect(routeHandler.joinGame(player2, onlineUsers, 'GAME1', activeGames)).toMatch('OK')
+		}
+		expect(routeHandler.joinGame(player3, onlineUsers, 'GAME2', activeGames)).toMatch('KO')
+	}
+})
+
+test('startGame', () => {
+	let player1 = { id : 0 }
+	let player2 = { id : 1 }
+	let player3 = { id : 2 }
+	let player4 = { id : 3 }
+
+	let logged1 = routeHandler.login(player1, player1, onlineUsers)
+	let logged2 = routeHandler.login(player2, player2, onlineUsers)
+	let logged3 = routeHandler.login(player3, player3, onlineUsers)
+	let logged4 = routeHandler.login(player4, player4, onlineUsers)
+
+	if (logged1 === 'OK' && logged2 === 'OK') {
+		let game = routeHandler.createGame(player1, activeGames, onlineUsers, 'GAME1')
+
+		if (game === 'OK') {
+			expect(routeHandler.joinGame(player2, onlineUsers, 'GAME1', activeGames)).toMatch('OK')
+			expect(routeHandler.startGame(player1, activeGames)).toMatchSnapshot()
+		}
+	}
+	if (logged3 === 'OK' && logged4 === 'OK') {
+		let game = routeHandler.createGame(player3, activeGames, onlineUsers, 'GAME2')
+
+		if(game === 'OK') {
+			routeHandler.joinGame(player4, onlineUsers, 'GAME2', activeGames)
+			expect(routeHandler.startGame(player4, activeGames)).toMatchSnapshot()
+		}
+	}
+})
+
+test('leaveGame', () => {
+	let player1 = { id : 0 }
+	let player2 = { id : 1 }
+	let player3 = { id : 2 }
+	let player4 = { id : 3 }
+	let player5 = { id : 4 }
+
+	let logged1 = routeHandler.login(player1, player1, onlineUsers)
+	let logged2 = routeHandler.login(player2, player2, onlineUsers)
+	let logged3 = routeHandler.login(player3, player3, onlineUsers)
+	let logged4 = routeHandler.login(player4, player4, onlineUsers)
+
+	if (logged1 === 'OK' && logged2 === 'OK') {
+		let game = routeHandler.createGame(player1, activeGames, onlineUsers, 'GAME1')
+
+		if (game === 'OK') {
+			routeHandler.joinGame(player2, onlineUsers, 'GAME1', activeGames)
+			expect(routeHandler.leaveGame(player1, activeGames)).toMatchSnapshot()
+		}
+	}
+	if (logged3 === 'OK' && logged4 === 'OK') {
+		let game = routeHandler.createGame(player3, activeGames, onlineUsers, 'GAME2')
+
+		if(game === 'OK') {
+			routeHandler.joinGame(player4, onlineUsers, 'GAME2', activeGames)
+			expect(routeHandler.leaveGame(player4, activeGames)).toMatchSnapshot()
+		}
+	}
+
+	expect(routeHandler.leaveGame(player5, activeGames)).toMatchSnapshot()
 })
 
 test('requestShape', () => {
