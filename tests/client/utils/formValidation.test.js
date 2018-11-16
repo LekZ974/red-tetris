db441a357f48512c20ec9e1c03ae0b76abc2a206
Alexandre Hoareau <alexandrehoareau@MacBook-Pro-de-Alexandre.local>
2018-11-16 18:42:06 +0100

    fix test + refacto action with socket

--- tests/client/utils/formValidation.test.js
+++ tests/client/utils/formValidation.test.js
@@ -15,4 +15,28 @@ describe('>>>>>formValidation', () => {
     expect(required(['something', 'an other thing'])).to.equal(undefined)
     expect(required({1:'something', 2:'an other thing'})).to.equal(undefined)
   })
+  it('function minLength3 is empty', () => {
+    expect(minLength3()).to.equal('Ce champ est requis')
+    expect(minLength3(undefined)).to.equal('Ce champ est requis')
+    expect(minLength3('')).to.equal('Ce champ est requis')
+    expect(minLength3(null)).to.equal('Ce champ est requis')
+  })
+  it('function minLength3 is not empty', () => {
+    expect(minLength3(5)).to.equal(undefined)
+    expect(minLength3('a string')).to.equal(undefined)
+    expect(minLength3(['something', 'an other thing'])).to.equal(undefined)
+    expect(minLength3({1:'something', 2:'an other thing'})).to.equal(undefined)
+  })
+  it('function maxLegnth15 is empty', () => {
+    expect(maxLength15()).to.equal('Ce champ est requis')
+    expect(maxLength15(undefined)).to.equal('Ce champ est requis')
+    expect(maxLength15('')).to.equal('Ce champ est requis')
+    expect(maxLength15(null)).to.equal('Ce champ est requis')
+  })
+  it('function maxLegnth15 is not empty', () => {
+    expect(maxLength15(5)).to.equal(undefined)
+    expect(maxLength15('a string')).to.equal(undefined)
+    expect(maxLength15(['something', 'an other thing'])).to.equal(undefined)
+    expect(maxLength15({1:'something', 2:'an other thing'})).to.equal(undefined)
+  })
 })
