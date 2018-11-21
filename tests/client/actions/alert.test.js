a03e8f3846e09fc5fe33e9d221ba9724ed19bb5d
Alexandre Hoareau <alexandrehoareau@MacBook-Pro-de-Alexandre.local>
2018-11-21 12:26:51 +0100

    fix tests + update package.json for jest

--- tests/client/actions/alert.test.js
+++ tests/client/actions/alert.test.js
@@ -1,8 +1,8 @@
 import * as actions from "../../../src/client/actions/alert"
-import { ALERT_POP } from "../../../src/client/actions/alert"
+import { DISPLAY_COMMAND } from "../../../src/client/actions/alert"
 
 describe('test alert action', ()=>{
   it('should test alert function', ()=>{
-    expect(actions.alert('salut')).toEqual({type:ALERT_POP, message:'salut'})
+    expect(actions.displayCommand()).toEqual({type:DISPLAY_COMMAND})
   })
 })
