c26ec3e8b934163d81a6499cc25ef3c7827e9455
kernel_j <jenniferwong89@gmail.com>
2018-12-22 12:38:10 +0100

    rework spectre functions

--- src/server/controllers/spectre.js
+++ src/server/controllers/spectre.js
@@ -1,3 +1,5 @@
+import board from '../constants/board'
+
 export default class Spectre {
 	constructor(board) {
 		this.spectre = board
@@ -11,20 +13,44 @@ export default class Spectre {
 		}
 	}
 
-    fillSpectreCol(row, col) {
-        for (let i = row; i < this.spectre.length; i++) {
-            this.spectre[i][col] = 1
+    fillMalus(numMalus) {
+        if (numMalus > 0) {
+            let row = this.spectre.length - 1
+            for (let i = numMalus; i > 0; i--) {
+                for (let col = 0; col < this.spectre[row].length; col++) {
+                    this.spectre[row][col] = -1
+                }
+                row--
+            }
         }
     }
 
-	generateSpectre(playerGrid) {
-        this.clearSpectre();
+    fillSpectreCol(row, col) {
+        if (row < 0) {
+            row = 0
+        }
+        for (let i = row; i < this.spectre.length; i++) {
+            if (this.spectre[i][col] !== -1)
+                this.spectre[i][col] = 1
+        }
+    }
+
+	generateSpectre(playerGrid, malus) {
+        if (!playerGrid || malus < 0)
+            return this.spectre
+        this.clearSpectre()
+        this.fillMalus(malus)
+
+        let rowsFilled = 0
 		for (let i = 0; i < playerGrid.length; i++) {
 			for (let j = 0; j < playerGrid[i].length; j++) {
 				if (playerGrid[i][j] > 0) {
-                    this.fillSpectreCol(i, j)
+                    this.fillSpectreCol(i - malus, j)
+                    rowsFilled++
                 }
 			}
+            if (rowsFilled === board.WIDTH)
+                break
 		}
         return this.spectre
 	}
