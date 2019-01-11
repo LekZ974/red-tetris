b053bec8652d01f760dc8014427cebd9e57af4d1
Alexandre HOAREAU <ahoareau@e2r5p17.42.fr>
2019-01-11 18:10:46 +0100

    wip gameMode solo multi

--- src/client/actions/alert.js
+++ src/client/actions/alert.js
@@ -1,8 +1,15 @@
-export const DISPLAY_COMMAND = 'DISPLAY_COMMAND'
+export const DISPLAY_COMMAND = 'alert/DISPLAY_COMMAND'
+export const DISPLAY_CONFIG_FORM = 'alert/DISPLAY_CONFIG_FORM'
 
 export const displayCommand = () => {
   return {
     type: DISPLAY_COMMAND,
+  }
+}
+
+export const displayConfigForm = () => {
+  return {
+    type: DISPLAY_CONFIG_FORM,
   }
 }
 
