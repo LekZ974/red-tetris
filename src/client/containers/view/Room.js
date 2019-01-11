08c53c6507699470aba85bfe0f5f38a6b808fcdc
Alexandre HOAREAU <ahoareau@e2r5p17.42.fr>
2019-01-11 18:10:46 +0100

    wip gameMode solo multi

--- src/client/containers/view/Room.js
+++ src/client/containers/view/Room.js
@@ -3,26 +3,39 @@ import { withRouter } from 'react-router-dom'
 import {Box, Card, LoadingContainer, Toaster} from '../../components/block'
 import Fade from 'react-reveal/Fade';
 import GridLoader from 'react-spinners/GridLoader';
-import Sound from 'react-sound';
-
-import musicFile from '../../assets/sounds/tetris-gameboy-02.mp3';
 
 import RoomInfo from '../../components/Room/RoomInfo'
 import GameInfo from '../../components/Room/GameInfo'
 import PlayGround from '../../components/Room/PlayGround'
 import {connect} from "react-redux";
-import {emitCreateGame, emitGameStatus, gameSound} from "../../actions/game";
-import {emitLogin, emitLeaveGame} from "../../actions/user";
-import {displayCommand} from "../../actions/alert";
+import {emitCreateGame, emitGameStatus} from "../../actions/game";
+import {emitLogin, emitLeaveGame, emitJoinGame} from "../../actions/user";
+import {displayCommand, displayConfigForm} from "../../actions/alert";
+import Modal from "../../components/block/Modal";
+import ConfigForm from "../form/ConfigForm";
+import { emitGetGames } from '../../actions/games';
+import {gameExist} from '../../utils/eventHandler';
 
 const Room = (props) => {
-  const {user, game, match, createGame, login} = props
+  const {user, game, gamesList, match, login, showConfigForm, displayConfigForm, getGames, createGame} = props
+
+  if (!gamesList && !user.name && !user.isLoading) {
+    getGames()
+  }
 
   if (!user.name && !user.isLoading) {
     login(match.params.user)
   }
 
-  if (!game.name && !game.isLoading) {
+  if (!gameExist(match.params.room, gamesList)) {
+    if (!game.name && !game.isLoading) {
+      return (
+        <Modal open={showConfigForm} onClose={displayConfigForm}>
+          <ConfigForm {...props} />
+        </Modal>
+      )
+    }
+  } else if (!game.name && !game.isLoading) {
     createGame(match.params.room)
   }
 
@@ -56,11 +69,6 @@ const Room = (props) => {
         </Box>
       </Fade>
     </LoadingContainer>
-    <Sound
-      url={musicFile}
-      playStatus={game.start && game.params.sound ? 'PLAYING' : 'STOPPED'}
-      loop={true}
-    />
   </Box>
   )
 }
@@ -68,11 +76,13 @@ const Room = (props) => {
 const mapDispatchToProps = dispatch => ({
   login: userName => dispatch(emitLogin(userName)),
   createGame: gameName => dispatch(emitCreateGame(gameName)),
+  joinGame: (userName, gameName) => dispatch(emitJoinGame(userName, gameName)),
   updateGameStatus: (status, game) => dispatch(emitGameStatus(status, game)),
   updateGame: data => dispatch(updateGame(data)),
   leaveGame: () => dispatch(emitLeaveGame()),
   displayCommand: () => dispatch(displayCommand()),
-  gameSound: (status) => dispatch(gameSound(status)),
+  displayConfigForm: () => dispatch(displayConfigForm()),
+  getGames: () => dispatch(emitGetGames()),
 })
 
 const mapStateToProps = state => {
@@ -83,8 +93,10 @@ const mapStateToProps = state => {
 
   return {
     user: state.user,
+    gamesList: state.games.items,
     game: Object.assign({}, state.game),
-    showCommand: state.alert.showCommand
+    showCommand: state.alert.showCommand,
+    showConfigForm: state.alert.showConfigForm
   }
 };
 
