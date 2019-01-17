2e7eaabb9df142ced4a4963dde0bf055461f5870
LekZ974 <ahoareau@student.42.fr>
2019-01-17 14:08:05 +0100

    Update alert.js

--- src/client/actions/alert.js
+++ src/client/actions/alert.js
@@ -1,7 +1,5 @@
 export const DISPLAY_COMMAND = 'alert/DISPLAY_COMMAND'
 export const DISPLAY_CONFIG_FORM = 'alert/DISPLAY_CONFIG_FORM'
-export const DISPLAY_RESULT = 'alert/DISPLAY_RESULT'
-export const ALERT_INIT = 'alert/ALERT_INIT'
 
 export const displayCommand = () => {
   return {
