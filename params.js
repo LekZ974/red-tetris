65d68fe37cdacb181278f87d92c6cef607d891e9
LekZ974 <ahoareau@student.42.fr>
2019-01-31 19:18:06 +0100

    Update params.js

--- params.js
+++ params.js
@@ -1,7 +1,7 @@
 const params = {
   server:{
      host: '0.0.0.0'
-   , socket: '0.0.0.0'
+   , socket: '192.168.99.100'
    , port: 3004
    , get url(){ return 'http://' + this.host + ':' + this.port }
    , get socketUrl(){ return 'http://' + this.socket + ':' + this.port }
